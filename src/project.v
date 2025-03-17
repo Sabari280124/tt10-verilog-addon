4aa25325dcf02b23e41b4f1a3356126fa02fa7f7
Sabari280124 <sabarigopakumar8@gmail.com>
2025-03-17 20:08:07 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,6 +2,7 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
+
 `default_nettype none
 
 module tt_um_cla (
@@ -25,27 +26,8 @@ module tt_um_cla (
 
     // Propagate and Generate logic
     assign P = A ^ B;           // Propagate
-    assign G = A & B;           // Generate
+    assign G = A & B;           // Gen
 
-    // Carry computation
-    assign C[0] = Cin;
-    assign C[1] = G[0] | (P[0] & C[0]);
-    assign C[2] = G[1] | (P[1] & C[1]);
-    assign C[3] = G[2] | (P[2] & C[2]);
-    assign Cout = G[3] | (P[3] & C[3]);
-
-    // Sum computation
-    assign Sum = P ^ C;
-
-    // Map outputs
-    assign uo_out = Sum;        // Map Sum to uo_out
-    assign uio_out = 1'b0;      // Unused output
-    assign uio_oe = 1'b0;       // Unused enable
-
-    // Prevent warnings for unused inputs
-    wire _unused = &{ena, clk, rst_n};
-
-endmodule
 
 
 
